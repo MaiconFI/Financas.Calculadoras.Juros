c82d1e3c985142bcbc4fa093997dd207335dbc89
Maicon Fischer Izidoro <maicaofischer@gmail.com>
2020-03-11 23:54:00 -0300

    Criado projeto de dtos

---
+++ src/Financas.Calculadoras.Juros.Dtos/CalculoDeJuros/CalculoDeJurosDto.cs
@@ -0,0 +1,9 @@
+﻿using Financas.Calculadoras.Juros.Dtos.Base;
+
+namespace Financas.Calculadoras.Juros.Dtos.CalculoDeJuros
+{
+    public class CalculoDeJurosDto : BaseDto
+    {
+        public decimal Resultado { get; set; }
+    }
+}
---
+++ src/Financas.Calculadoras.Juros.Dtos/TaxaDeJuros/TaxaDeJurosDto.cs
@@ -0,0 +1,9 @@
+﻿using Financas.Calculadoras.Juros.Dtos.Base;
+
+namespace Financas.Calculadoras.Juros.Dtos.TaxaDeJuros
+{
+    public class TaxaDeJurosDto : BaseDto
+    {
+        public decimal Valor { get; set; }
+    }
+}
