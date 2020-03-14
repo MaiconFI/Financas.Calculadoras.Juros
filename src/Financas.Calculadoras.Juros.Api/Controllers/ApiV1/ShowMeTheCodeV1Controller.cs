0ed004d5d30807c072bc8959d1b0055bb250157e
Maicon Fischer Izidoro <maicaofischer@gmail.com>
2020-03-14 10:37:35 -0300

    Criado endpoint para retornar os caminhos no github e dockerhub da aplicação

---
+++ src/Financas.Calculadoras.Juros.Api/Controllers/ApiV1/ShowMeTheCodeV1Controller.cs
@@ -0,0 +1,14 @@
+﻿using Financas.Calculadoras.Juros.Api.Controllers.Base.ApiV1;
+using Financas.Calculadoras.Juros.Api.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Financas.Calculadoras.Juros.Api.Controllers.ApiV1
+{
+    [Route("v{version:apiVersion}/showmethecode")]
+    public class ShowMeTheCodeV1Controller : ApiV1BaseController
+    {
+        [HttpGet]
+        public IActionResult Get()
+            => Ok(new ShowMeTheCodeViewModel());
+    }
+}
---
+++ src/Financas.Calculadoras.Juros.Api/ViewModels/ShowMeTheCodeViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Financas.Calculadoras.Juros.Api.ViewModels
+{
+    public class ShowMeTheCodeViewModel
+    {
+        public string UrlDockerHubCalculadoraDeJuros => "https://hub.docker.com/r/maiconfi/financas-calculadora-juros";
+        public string UrlDockerHubTaxaDeJuros => "https://hub.docker.com/r/maiconfi/financas-taxas-juros";
+        public string UrlGitHubCalculadoraDeJuros => "https://github.com/MaiconFI/Financas.Calculadoras.Juros";
+        public string UrlGitHubTaxaDeJuros => "https://github.com/MaiconFI/Financas.Taxas.Juros";
+    }
+}
